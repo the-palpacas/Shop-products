67606f4c986b8c9d13ff321ab6bb4e28578a6e7b
Xue Chen <xueqingc@gmail.com>
2018-06-07 17:31:08 -0700

    add styling to product images and names

--- client/src/components/productInfo.jsx
+++ client/src/components/productInfo.jsx
@@ -5,7 +5,7 @@ const ProductInfo = (props) => {
     <div className="productInfo">
       <img className="img-thumbnail" src={props.prodInfo[1].imgs_url[0]} />
       <div>
-        <div className="bold" name="productName">{props.prodInfo[0]}</div>
+        <div className="bold text-truncate" name="productName">{props.prodInfo[0]}</div>
         <div className="price" name="price">${props.prodInfo[1].price} <span className="shippingInfo">Free shipping</span></div>
       </div>
     </div>
