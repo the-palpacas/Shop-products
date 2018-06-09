fe176c3c1c50cebd19be86cb18944e4cbdd406dd
Xue Chen <xueqingc@gmail.com>
2018-06-09 12:42:45 -0700

    edits based on review suggestions.

--- client/src/components/photoCarousel.jsx
+++ client/src/components/photoCarousel.jsx
@@ -2,9 +2,8 @@ import React from 'react';
 
 const PhotoCarousel = (props) => {
   const carouselId = `carousel${props.prodId}`;
-  const hashCarouselId = `#${carouselId}`;
   const slides = props.imgs_url.map((url, index) => {
-    let altText = `Slice ${index + 1}`;
+    let altText = `Slide ${index + 1}`;
     if (index === 0) {
       return (
         <div className="carousel-item active" key={index.toString()} >
@@ -21,9 +20,9 @@ const PhotoCarousel = (props) => {
 
   const indicators = props.imgs_url.map((url, index) => {
     if (index === 0) {
-      return <li data-target={hashCarouselId} data-slide-to={index} className="active" key={index.toString()} />;
+      return <li data-target={`#${carouselId}`} data-slide-to={index} className="active" key={index.toString()} />;
     }
-    return <li data-target={hashCarouselId} data-slide-to={index} key={index.toString()} />;
+    return <li data-target={`#${carouselId}`} data-slide-to={index} key={index.toString()} />;
   });
 
   return (
@@ -34,11 +33,11 @@ const PhotoCarousel = (props) => {
       <div className="carousel-inner">
         {slides}
       </div>
-      <a className="carousel-control-prev" href={hashCarouselId} role="button" data-slide="prev">
+      <a className="carousel-control-prev" href={`#${carouselId}`} role="button" data-slide="prev">
         <span className="carousel-control-prev-icon" aria-hidden="true" />
         <span className="sr-only">Previous</span>
       </a>
-      <a className="carousel-control-next" href={hashCarouselId} role="button" data-slide="next">
+      <a className="carousel-control-next" href={`#${carouselId}`} role="button" data-slide="next">
         <span className="carousel-control-next-icon" aria-hidden="true" />
         <span className="sr-only">Next</span>
       </a>
